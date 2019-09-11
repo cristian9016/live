3b487f18af5f09816e6f260278d3667b4c9e4eb3
Cristian David Muñoz - Ceiba Software <cristian.munoz@ceiba.com.co>
2019-09-11 12:33:00 -0500

    player working

---
+++ app/src/main/java/com/example/fertvlive/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.example.fertvlive
+
+import android.net.Uri
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.fertvlive.databinding.TemplateChangeUrlBinding
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.cancelButton
+import org.jetbrains.anko.yesButton
+
+class MainActivity : AppCompatActivity() {
+
+    var URI_PATH =
+        "https://go5lm4kolawb-hls-live.5centscdn.com/fertv/ab6c040066603ef2519d512b21dce9ab.sdp/index.m3u8"
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        load()
+
+        mainVideo.setOnErrorListener { _, _, _ ->
+            alert {
+                title = getString(R.string.error_loading)
+                positiveButton(getString(R.string.retry_text)) {
+                    load()
+                }
+                negativeButton(getString(R.string.close_text)){
+                    finish()
+                }
+            }.show()
+            true
+        }
+
+        mainVideo.setOnPreparedListener {
+            pbLoading.visibility = View.GONE
+        }
+
+        changeUrl.setOnClickListener {
+            changeUrl()
+        }
+
+    }
+
+    private fun load(){
+        pbLoading.visibility = View.VISIBLE
+        mainVideo.setVideoURI(Uri.parse(URI_PATH))
+        mainVideo.start()
+    }
+
+    private fun changeUrl() =
+        alert {
+            title = getString(R.string.change_url_text)
+            val binding: TemplateChangeUrlBinding =
+                DataBindingUtil.inflate(layoutInflater, R.layout.template_change_url, null, false)
+            customView = binding.root
+            yesButton {
+                URI_PATH = binding.urlText.toString()
+                mainVideo.stopPlayback()
+                mainVideo.setVideoURI(Uri.parse(URI_PATH))
+                mainVideo.start()
+            }
+            cancelButton { }
+        }.show()
+
+}
